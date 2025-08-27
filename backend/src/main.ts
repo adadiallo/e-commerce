238487e11ae7e958231d0adcc9cb4be2fcdb2fc1
@ada_diallo <adadialo2203@gmail.com>
2025-08-27 21:31:31 +0000

    essai

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//     app.enableCors({
+//     origin: 'http://localhost:3001', // ou '*' en développement
+//     credentials: true,
+//   });
+//     app.use('/webhook', bodyParser.raw({ type: 'application/json' }));
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
-    origin: 'http://localhost:3001', // ou '*' en développement
-    credentials: true,
-  });
-    app.use('/webhook', bodyParser.raw({ type: 'application/json' }));
-
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors(); // autorise les requêtes du frontend
+  await app.listen(3000);
 }
 bootstrap();
