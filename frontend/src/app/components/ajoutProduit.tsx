3df7504f3f72c54c6071af211806955fa7029370
@ada_diallo <adadialo2203@gmail.com>
2025-09-02 20:15:04 +0000

    ajout navbar et sidebar listeproduits

--- frontend/src/app/components/ajoutProduit.tsx
+++ frontend/src/app/components/ajoutProduit.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { FiEdit, FiTrash, FiPlus } from "react-icons/fi";
 import toast from "react-hot-toast";
+import DashboardCards from "../countTable/page";
 
 type Categorie = {
   id: number;
@@ -140,6 +141,8 @@ const handleSubmit = async (e: React.FormEvent) => {
 
 
   return (
+    <>
+    <DashboardCards/>
     <div className="max-w-5xl mx-auto mt-10 p-4 text-sm">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-semibold text-[#0c5e69]">Liste des produits</h2>
@@ -285,5 +288,6 @@ const handleSubmit = async (e: React.FormEvent) => {
         </div>
       )}
     </div>
+    </>
   );
 }
