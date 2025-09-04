d6f2c00f9f20090f978c158fa143f1d3bb65e611
@ada_diallo <adadialo2203@gmail.com>
2025-09-04 10:31:26 +0000

    ajout produitManager dans dahboard2

--- frontend/src/app/admin/produits/page.tsx
+++ frontend/src/app/admin/produits/page.tsx
@@ -1,12 +1,12 @@
 
 
-// import ListeProduits from "@/app/components/listeProduit";
-// import DashboardPage from "@/app/dashboard/page";
+import ListeProduits from "@/app/components/listeProduit";
+import DashboardPage from "@/app/dashboard/page";
 
 
 
-// export default function AdminProduitsPage() {
-//   return (
-//     <DashboardPage/>
-//   );
-// }
+export default function AdminProduitsPage() {
+  return (
+    <DashboardPage/>
+  );
+}
